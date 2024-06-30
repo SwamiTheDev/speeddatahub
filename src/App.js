1e79907f30bc4085e2dcb733c1f0977ebe59930c
swamithedev-admin <swamithedev@gmail.com>
2024-06-30 20:26:45 +0530

    remove old newly implemented the layout component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
-import Internet from "./component/InternetInfo/internet";
+import Layout from "./Layout/layout";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="row">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5 col-xxl-5">
-            <Internet />
-          </div>
-          <div className="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5 col-xxl-5"></div>
-        </div>
+        <Layout />
       </div>
     </div>
   );
