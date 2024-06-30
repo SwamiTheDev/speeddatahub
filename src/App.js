15d090f8c7e35f9b5f271c9cc4af937e237d48ee
swamithedev-admin <swamithedev@gmail.com>
2024-06-30 11:59:51 +0530

    remove network folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Internet from "./component/InternetInfo/internet";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5 col-xxl-5">
+            <Internet />
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5 col-xxl-5"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
