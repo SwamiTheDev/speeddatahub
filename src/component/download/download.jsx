ce39b8acf0d408af015e3c53f0b9725bab766914
swamithedev-admin <swamithedev@gmail.com>
2024-06-30 20:26:14 +0530

    ""

--- src/component/download/download.jsx
+++ src/component/download/download.jsx
@@ -1,8 +1,21 @@
-import './download.css';
+import React from 'react';
+import ReactSpeedometer from "react-d3-speedometer";
 
-export default function Download() {
+const Download = ({ value }) => {
     return (
-        <>
-        </>
-    )
-}
+        <div className="speedometer">
+            <h2>Download Speed</h2>
+            <ReactSpeedometer
+                className='meter'
+                maxValue={100}
+                value={value}
+                needleTransitionDuration={1000}
+                needleTransition="easeElastic"
+                currentValueText={`${value} Mbps`}
+                segmentColors={["#007fff", "#FAFAFA"]}
+            />
+        </div>
+    );
+};
+
+export default Download;
