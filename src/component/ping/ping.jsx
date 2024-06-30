1163355f6ca0f411c5bd1805d8d88fd365812f5e
swamithedev-admin <swamithedev@gmail.com>
2024-06-30 20:25:44 +0530

    apply the meter and api

--- src/component/ping/ping.jsx
+++ src/component/ping/ping.jsx
@@ -1,8 +1,22 @@
-import './ping.css';
+import React from 'react';
+import ReactSpeedometer from "react-d3-speedometer";
+import './ping.css'
 
-export default function Ping() {
+const Ping = ({ value }) => {
     return (
-        <>
-        </>
-    )
-}
+        <div className="speedometer">
+            <h2>Ping Speed</h2>
+            <ReactSpeedometer
+                maxValue={100}
+                className='meter'
+                value={value}
+                needleTransitionDuration={1000}
+                needleTransition="easeElastic"
+                currentValueText={`${value} ms`}
+                segmentColors={["#007fff", "#FAFAFA"]}
+            />
+        </div>
+    );
+};
+
+export default Ping;
