e19c31084a99049318c6eba930a144bd4bb1fea3
swamithedev-admin <swamithedev@gmail.com>
2024-07-03 18:25:05 +0530

    update the layout using grid

--- src/Layout/layout.jsx
+++ src/Layout/layout.jsx
@@ -1,6 +1,3 @@
-import Internet from "../component/InternetInfo/internet";
-// import Meter from "../component/Meter/meter";
-import Download from "../component/download/download";
 import SpeedTest from "../component/speedtest/speedtest";
 
 export default function Layout() {
@@ -11,17 +8,8 @@ export default function Layout() {
                     <SpeedTest />
                 </div>
                 <div className="row">
-                    {/* internet info */}
-                    <div className="col">
-                        <Internet />
-                    </div>
-
-                </div>
-                <div className="row">
                     <div className="col">
                     </div>
-                    <div className="col"></div>
-                    <div className="col"></div>
                 </div>
             </div>
         </>
