9f8501740ffd213ae96cc320a76ca99abf79b653
swamithedev-admin <swamithedev@gmail.com>
2024-07-05 00:30:51 +0530

    set the fixed page size

--- src/Layout/layout.jsx
+++ src/Layout/layout.jsx
@@ -1,10 +1,11 @@
 import SpeedTest from "../component/speedtest/speedtest";
+import './layout.css'
 
 export default function Layout() {
     return (
         <>
-            <div className="grid">
-                <div className="row ">
+            <div className="row ">
+                <div className="page">
                     <SpeedTest />
                 </div>
                 <div className="row">
