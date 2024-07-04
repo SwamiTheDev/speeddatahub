12aecd6cae323c6df3f3842c8b81e6fe8182a0c0
swamithedev-admin <swamithedev@gmail.com>
2024-07-05 00:31:16 +0530

    apply a box design code

--- src/component/box/box.jsx
+++ src/component/box/box.jsx
@@ -3,7 +3,7 @@ import boxstyle from './box.module.css'
 export default function Box(props) {
     return (
         <>
-            <div className=''>
+            <div className={boxstyle.box}>
                 <p className={boxstyle.title}>{props.title}</p>
                 <p className={boxstyle.speed}>{props.speed} </p>
                 <p class={boxstyle.value}>{props.value}</p>
