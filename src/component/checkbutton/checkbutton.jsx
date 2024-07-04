bc7f54452ab33595c3b174f61dce710ec0f90c42
swamithedev-admin <swamithedev@gmail.com>
2024-07-05 00:32:06 +0530

    go button ui change

--- src/component/checkbutton/checkbutton.jsx
+++ src/component/checkbutton/checkbutton.jsx
@@ -3,11 +3,27 @@ import './checkbutton.css'
 export default function Checkbutton(props) {
     return (
         <>
-            <div className='button-container'>
+            {/* <div className='button-container'>
                 <button onClick={props.function} className="effect1_alt">
-                    <span className="label">check</span>
+                    <span className="label">Start</span>
                 </button>
+            </div> */}
+            <div id="wrapper" className='button-container'>
+                <a href="#" class="my-super-cool-btn">
+                    <div class="dots-container">
+                        <div class="dot"></div>
+                        <div class="dot"></div>
+                        <div class="dot"></div>
+                        <div class="dot"></div>
+                    </div>
+                    <span>Go!</span>
+                </a>
             </div>
+            {/* <div className="button-container">
+                <div className="pp">
+
+                </div>
+            </div> */}
         </>
     )
 }
