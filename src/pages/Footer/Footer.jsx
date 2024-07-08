867517d01911e44bb1ace9bed096b5548349aa6c
swamithedev-admin <swamithedev@gmail.com>
2024-07-09 00:48:54 +0530

    responsive apply in footer component

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,63 +1,63 @@
+import './Footer.css'
 export default function Footers() {
     return (
         <>
             {/* <!-- Remove the container if you want to extend the Footer to full width. --> */}
-            <div class="container my-5">
+            <div class="footer ">
                 {/* <!-- Footer --> */}
                 <footer
                     class="text-center text-lg-start text-dark"
-                    style={{ backgroundColor: "#ECEFF1" }}
+
                 >
                     {/* <!-- Section: Social media --> */}
-                    <section
-                        class="d-flex justify-content-between p-4 text-white"
-                        style={{ backgroundColor: "#21D192" }}
-                    >
-                        {/* <!-- Left --> */}
-                        <div class="me-5">
-                            <span>Get connected with us on social networks:</span>
-                        </div>
-                        {/* <!-- Left --> */}
+                    <div className="container">
+                        <section
+                            class="_footer_title d-flex justify-content-between p-4 text-white"
 
-                        {/* <!-- Right --> */}
-                        <div>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-google"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-instagram"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-linkedin"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-github"></i>
-                            </a>
-                        </div>
-                        {/* <!-- Right --> */}
-                    </section>
+                        >
+
+                            {/* <!-- Left --> */}
+                            <div class="me-5">
+                                <span className='_title_'>Get connected with us on social networks:</span>
+                            </div>
+                            {/* <!-- Left --> */}
+
+                            {/* <!-- Right --> */}
+                            <div>
+                                <a href="" class="_icons_ me-2">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                                <a href="" class="_icons_ me-2">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                                <a href="" class="_icons_ me-2">
+                                    <i class="fab fa-google"></i>
+                                </a>
+                                <a href="" class="_icons_ me-2">
+                                    <i class="fab fa-instagram"></i>
+                                </a>
+                                <a href="" class="_icons_ me-2">
+                                    <i class="fab fa-linkedin"></i>
+                                </a>
+                                <a href="" class="_icons_ me-2">
+                                    <i class="fab fa-github"></i>
+                                </a>
+                            </div>
+                        </section>
+                    </div>
+                    {/* <!-- Right --> */}
                     {/* <!-- Section: Social media --> */}
 
                     {/* <!-- Section: Links  --> */}
-                    <section class="">
-                        <div class="container text-center text-md-start mt-5">
+                    <section class="__lists">
+                        <div class="container text-md-start mt-5">
                             {/* <!-- Grid row --> */}
                             <div class="row mt-3">
                                 {/* <!-- Grid column --> */}
-                                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                                <div class="col-md-3 col-lg-4 col-xl-3  mb-4 ms-2">
                                     {/* <!-- Content --> */}
-                                    <h6 class="text-uppercase fw-bold">Company name</h6>
-                                    <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
-                                        style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
-                                    />
-                                    <p>
+                                    <h6 class="list_title text-uppercase __link ms-1">speedtesthub</h6>
+                                    <p className='list_ans'>
                                         Here you can use rows and columns to organize your footer
                                         content. Lorem ipsum dolor sit amet, consectetur adipisicing
                                         elit.
@@ -66,63 +66,64 @@ export default function Footers() {
                                 {/* <!-- Grid column --> */}
 
                                 {/* <!-- Grid column --> */}
-                                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                                <div class="col-md-2 col-lg-2 col-xl-2 mb-3 ms-3">
                                     {/* <!-- Links --> */}
-                                    <h6 class="text-uppercase fw-bold">Products</h6>
+                                    {/* <p>kndfs</p> */}
+                                    <h6 class="list_title __link">Products</h6>
                                     <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
+                                        class="mb-2 mt-0  "
                                         style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
                                     />
-                                    <p>
-                                        <a href="#!" class="text-dark">MDBootstrap</a>
+                                    <p class="__link mb-2">
+                                        <a href="#!" class="_link">MDBootstrap</a>
                                     </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">MDWordPress</a>
+                                    <p class="__link mb-2">
+                                        <a href="#!" class="_link">MDWordPress</a>
                                     </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">BrandFlow</a>
+                                    <p class="__link mb-2">
+                                        <a href="#!" class="_link">BrandFlow</a>
                                     </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">Bootstrap Angular</a>
+                                    <p class="__link mb-2">
+                                        <a href="#!" class="_link">Bootstrap Angular</a>
                                     </p>
                                 </div>
                                 {/* <!-- Grid column --> */}
 
                                 {/* <!-- Grid column --> */}
-                                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                <div class="col-md-3 col-lg-2 col-xl-2  ms-3 mb-1">
                                     {/* <!-- Links --> */}
-                                    <h6 class="text-uppercase fw-bold">Useful links</h6>
+                                    <h6 class="list_title __link">Useful links</h6>
                                     <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
-                                        style={{ width: ' 60px', backgroundColor: '#7c4dff', height: '2px' }}
+                                        class="mb-2 mt-0 "
+                                        style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
                                     />
-                                    <p>
-                                        <a href="#!" class="text-dark">Your Account</a>
+                                    <p class="__link mb-2">
+                                        <a href="#!" class="_link">Your Account</a>
                                     </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">Become an Affiliate</a>
+                                    <p class="__link mb-2">
+                                        <a href="#!" class="_link">Become an Affiliate</a>
                                     </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">Shipping Rates</a>
+                                    <p class="__link mb-2">
+                                        <a href="#!" class="_link">Shipping Rates</a>
                                     </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">Help</a>
+                                    <p class="__link mb-2">
+                                        <a href="#!" class="_link">Help</a>
                                     </p>
                                 </div>
                                 {/* <!-- Grid column --> */}
 
                                 {/* <!-- Grid column --> */}
-                                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                                <div class="col-md-4 col-lg-3 col-xl-3 mb-md-0 ms-3 mb-1">
                                     {/* <!-- Links --> */}
-                                    <h6 class="text-uppercase fw-bold">Contact</h6>
+                                    <h6 class="list_title __link">Contact</h6>
                                     <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
+                                        class="mb-2 mt-0 "
                                         style={{ width: '60px', backgroundColor: ' #7c4dff', height: '2px' }}
                                     />
-                                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                                    <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                                    <p className='__link mb-2'><i class="fas fa-home mr-5"></i> New York, NY 10012, US</p>
+                                    <p className='__link mb-2'><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+                                    <p className='__link mb-2'><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                                    <p className='__link mb-2'><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
                                 </div>
                                 {/* <!-- Grid column --> */}
                             </div>
@@ -142,9 +143,9 @@ export default function Footers() {
                         >
                     </div>
                     {/* <!-- Copyright --> */}
-                </footer>
+                </footer >
                 {/* <!-- Footer --> */}
-            </div>
+            </div >
             {/* <!-- End of .container --> */}
         </>
     )
